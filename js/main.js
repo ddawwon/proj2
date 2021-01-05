5c526cc7d769117a442513ed6c9079e379672d27
ddawwon <76501372+ddawwon@users.noreply.github.com>
2021-01-05 09:41:13 +0900

    210105_0941

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    //네비버튼
+    //slider
+    $('#nav_pop_btn').click(function(){
+        $('#nav_pop').fadeIn();
+    })
+    $('.btn_all').click(function(){
+        $('#nav_pop').fadeOut();
+    })
+
+})//jq
---
+++ js/slide.js
@@ -0,0 +1,38 @@
+$(function(){
+    $(".lazy").slick({
+        lazyLoad: 'ondemand', // ondemand progressive anticipated
+        infinite: true
+      });
+
+      $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      });
+
+      $(".center").slick({
+        dots: true,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 6,
+        slidesToScroll: 1
+      });
+
+
+      //패럴렉스
+      $(document).ready(function() {
+        var wd = $(window);
+        $('.paral').each(function(){
+          var bg = $(this);
+          wd.scroll(function(){
+            var yPos = -(wd.scrollTop() / 2); 
+              //나눠지는 숫자가 작을 수록 속도가 빠름
+            var coords = '50%' + yPos + 'px';
+            bg.css({backgroundPosition:coords});
+          });
+        });
+      });
+
+
+})//jq
